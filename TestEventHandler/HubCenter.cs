6668e960859e39e4b0f33486989f32f402d08f77
Rezaei Hamzeh - Software <hr96636@crouseco.com>
2024-09-19 11:40:22 +0330

    aaa

---
+++ TestEventHandler/HubCenter.cs
@@ -0,0 +1,16 @@
+﻿namespace TestEventHandler
+{
+    public class HubCenter
+    {
+        public event EventHandler Onchange;
+        public event EventHandler<int> Onreceive;
+        public async Task CallHub()
+        {
+            Onchange.Invoke(this, EventArgs.Empty);
+        }
+        public async Task CallHub2(int id)
+        {
+            Onreceive.Invoke(this,id);
+        }
+    }
+}
